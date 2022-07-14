ece9651cbc82e4a7fa1d02ba4411b0888b833e21
nocrtsj <108843042+nocrtsj@users.noreply.github.com>
2022-07-14 13:02:19 +0800

    Update localtv.php

--- localtv.php
+++ localtv.php
@@ -1,4 +1,4 @@
 <?php
   $token = file_get_contents("localtv_token.txt");
   header("Location: http://103.105.213.251:8443" . $token . $_GET["channel"] . ".m3u8");
-//end https://raw.githubusercontent.com/jmvbambico/iptv-sniper/master/localtv.php
+//end https://iptv-sniper.herokuapp.com/localtv.php
